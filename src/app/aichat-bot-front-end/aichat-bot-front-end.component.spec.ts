6f8140d21a709dd5943a39dd42e7678a08e22d04
Abdurraffay <abdurraffaysyed@gmail.com>
2026-01-02 18:21:51 +0100

    Bug #6 resolved.

--- src/app/aichat-bot-front-end/aichat-bot-front-end.component.spec.ts
+++ src/app/aichat-bot-front-end/aichat-bot-front-end.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IGX_LIST_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective } from 'igniteui-angular';
 import { AIChatBotFrontEndComponent } from './aichat-bot-front-end.component';
@@ -11,7 +12,7 @@ describe('AIChatBotFrontEndComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AIChatBotFrontEndComponent, NoopAnimationsModule, FormsModule, ReactiveFormsModule, IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IGX_LIST_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective]
+      imports: [AIChatBotFrontEndComponent, NoopAnimationsModule, HttpClientTestingModule, FormsModule, ReactiveFormsModule, IGX_NAVBAR_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IGX_LIST_DIRECTIVES, IGX_INPUT_GROUP_DIRECTIVES, IgxButtonDirective]
     })
     .compileComponents();
 
--- src/app/aichat-bot-front-end/aichat-bot-front-end.component.ts
+++ src/app/aichat-bot-front-end/aichat-bot-front-end.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IGX_INPUT_GROUP_DIRECTIVES, IGX_LIST_DIRECTIVES, IGX_NAVBAR_DIRECTIVES, IgxButtonDirective, IgxIconButtonDirective, IgxIconComponent } from 'igniteui-angular';
+import { ChatService } from '../chat.service';
 
 @Component({
+  standalone: true,
   selector: 'app-aichat-bot-front-end',
-  imports: [IGX_INPUT_GROUP_DIRECTIVES, IGX_NAVBAR_DIRECTIVES, IGX_LIST_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IgxButtonDirective],
+  imports: [IGX_INPUT_GROUP_DIRECTIVES, IGX_NAVBAR_DIRECTIVES, IGX_LIST_DIRECTIVES, IgxIconButtonDirective, IgxIconComponent, IgxButtonDirective, FormsModule],
   templateUrl: './aichat-bot-front-end.component.html',
   styleUrls: ['./aichat-bot-front-end.component.scss']
 })
 export class AIChatBotFrontEndComponent {
   public chatHistorySelectedItem?: string;
+  public message: string = '';
+
+  constructor(private chatService: ChatService) {}
+
+  onSubmit(): void {
+    const text = this.message?.trim();
+    if (!text) {
+      return;
+    }
+
+    this.chatService.sendMessage(text).subscribe({
+      next: (response) => {
+        // TODO: handle response and update UI (chat history etc.)
+        console.log('Chat response:', response);
+      },
+      error: (err) => console.error('Send message failed', err)
+    });
+
+    // clear input after sending
+    this.message = '';
+  }
 }
